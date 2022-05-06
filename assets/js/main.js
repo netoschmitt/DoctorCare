e759fda137663e222756c13992ebfb5a7a8f6c17
neto schmitt <agneloschmittneto@gmail.com>
2022-05-06 14:06:45 -0300

    css customizaçao com --hue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,25 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
 function onScroll() {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
     if(scrollY > 0) {
         navigation.classList.add('scroll')
     } else {
         navigation.classList.remove('scroll')
+    }
+}
+
+function showBackToTopButtonOnScroll() {
+    if(scrollY > 500) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
     }
 }
 
@@ -13,7 +30,6 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove('menu-expanded')
 }
-
 
 /* lib */
 ScrollReveal({
